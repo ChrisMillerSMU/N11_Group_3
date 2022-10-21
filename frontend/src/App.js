dde4eb0dd2e29640df7a98e500e73d01af480330
Christopher Miller <chrismiller@smu.edu>
2022-10-20 20:01:28 -0500

    Minor fixes for EC2
    Complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,9 +9,9 @@ function App () {
   const [values, setValues] = useState([])
 
   // ENTER YOUR EC2 PUBLIC IP/URL HERE
-  const ec2_url = ''
+  const ec2_url = 'ec2-34-215-125-143.us-west-2.compute.amazonaws.com'
   // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
-  const ec2 = false;
+  const ec2 = true;
   // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
   const url = ec2 ? ec2_url : 'localhost'
 
