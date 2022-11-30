ad9fda3e7d32a9efb250f9d965ed8b24cb96016a
melodie-z <71799620+melodie-z@users.noreply.github.com>
2022-11-29 18:38:21 -0600

    Changed variable names

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -3,19 +3,41 @@ const User = require('../models/users' );
 const Athlete = require('../models/athletes' );
 const Company = require('../models/companies' );
 
-const accessTokenSecret = 'mysupercoolsecret' ; // more cryptographic key - environmental variable
+const accessTokenSecret = 'key' ;
 
 const authenticateAthlete = async (email, password ) => {
     const user = await User.authenticateUser (email, password );
     if (user === null) {
         return user;
     }
+<<<<<<< Updated upstream
     const students = await Athlete.findUserByEmail (email);
     console.log('Athletes' , athletes );
     const accessToken = jwt.sign({ ...athletes [0], claims: ['athlete' ] }, accessTokenSecret );
+=======
+    const athletes = await Athlete.findUserByEmail (email);
+    console.log('Athletes' , athletes );
+    const accessToken = jwt.sign({ ...athletes [0], claims: ['athlete'] }, accessTokenSecret );
+    
+    return accessToken;
+}
+const authenticateCompany = async (email, password ) => {
+    const user = await User.authenticateUser (email, password );
+    if (user === null) {
+        return user;
+    }
+    const companies = await Company.findUserByEmail (email);
+    console.log('Companies' , companies );
+    const accessToken = jwt.sign({ ...companies [0], claims: ['company' ] }, accessTokenSecret );
+>>>>>>> Stashed changes
     
     return accessToken;
 }
 module.exports = {
+<<<<<<< Updated upstream
     authenticateAthlete
+=======
+    authenticateAthlete,
+    authenticateCompany
+>>>>>>> Stashed changes
 };
